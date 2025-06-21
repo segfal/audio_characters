ad5893704238499e1830ed87af0ee6a4cfa5f7c6
segfal <segfal@adminsmacstudio.mynetworksettings.com>
2025-06-21 15:29:27 -0400

    add server change and python change

--- prototype/main.py
+++ prototype/main.py
@@ -1,2 +1,14 @@
 import requests
-print("SOUND IS PLAYING")
+import os
+from script_parser import parse_txt_script
+
+txt_scripts = [f"user_input_script/{i}" for i in os.listdir("user_input_script")]
+
+
+for txt in txt_scripts:
+    print(
+        parse_txt_script(input_txt=txt)
+    )
+
+
+
